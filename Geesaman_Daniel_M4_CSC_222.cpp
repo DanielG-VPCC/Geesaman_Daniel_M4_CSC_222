18e3a02c7388aa5d82cf2abe9caa58ac926fa192
Daniel <Daniel@GERALD>
2025-04-23 16:55:18 -0400

    Add project files.

---
+++ Geesaman_Daniel_M4_CSC_222.cpp
@@ -0,0 +1,9 @@
+// Geesaman_Daniel_M4_CSC_222.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
